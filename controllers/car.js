8abe4129d2b00fa7be579b176358f67bdd6a32cc
Sai Dinesh Kopparthi <S559963@nwmissouri.edu>
2023-04-12 12:40:16 -0500

    POST update

--- controllers/car.js
+++ controllers/car.js
@@ -31,9 +31,30 @@ exports.car_view_all_Page = async function (req, res) {
 exports.car_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Car detail: ' + req.params.id);
 };
-// Handle Car create on POST.
-exports.car_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Car create POST');
+// // Handle Car create on POST.
+// exports.car_create_post = function (req, res) {
+//     res.send('NOT IMPLEMENTED: Car create POST');
+// };
+
+// Handle Costume create on POST.
+exports.car_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Car();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"car_name":"Charger", "car_make":"Dodge", "car_cost":30000}
+    document.car_name = req.body.car_name;
+    document.car_make = req.body.car_make;
+    document.car_cost = req.body.car_cost;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // Handle Car delete form on DELETE.
 exports.car_delete = function (req, res) {
