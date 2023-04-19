9b202dfad5c4684f91d6ad41cd18912f24af1cd1
Sai Dinesh Kopparthi <S559963@nwmissouri.edu>
2023-04-19 13:02:19 -0500

    update and delete view added

--- controllers/car.js
+++ controllers/car.js
@@ -140,3 +140,32 @@ exports.car_create_Page = function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for updating a car.
+// query provides the id
+exports.car_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Car.findById(req.query.id)
+        res.render('carupdate', { title: 'Car Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.car_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Car.findById(req.query.id)
+        res.render('cardelete', {
+            title: 'Car Delete', toShow: result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/car.js
+++ routes/car.js
@@ -20,4 +20,10 @@ router.get('/detail', car_controlers.car_view_one_Page);
 /* GET create car page */
 router.get('/create', car_controlers.car_create_Page);
 
+/* GET create update page */
+router.get('/update', car_controlers.car_update_Page);
+
+/* GET delete car page */
+router.get('/delete', car_controlers.car_delete_Page);
+
 module.exports = router;
