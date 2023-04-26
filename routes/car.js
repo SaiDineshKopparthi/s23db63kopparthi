91fe6149db48bf893b1882f5f9187121a365732e
Sai Dinesh Kopparthi <S559963@nwmissouri.edu>
2023-04-25 21:53:12 -0500

    authentication for update page

--- routes/car.js
+++ routes/car.js
@@ -9,10 +9,10 @@
 // module.exports = router;
 
 var express = require('express');
-const car_controlers= require('../controllers/car');
+const car_controlers = require('../controllers/car');
 var router = express.Router();
 /* GET costumes */
-router.get('/', car_controlers.car_view_all_Page );
+router.get('/', car_controlers.car_view_all_Page);
 
 /* GET detail car page */
 router.get('/detail', car_controlers.car_view_one_Page);
@@ -20,8 +20,19 @@ router.get('/detail', car_controlers.car_view_one_Page);
 /* GET create car page */
 router.get('/create', car_controlers.car_create_Page);
 
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET create update page */
-router.get('/update', car_controlers.car_update_Page);
+router.get('/update', secured, car_controlers.car_update_Page);
 
 /* GET delete car page */
 router.get('/delete', car_controlers.car_delete_Page);
