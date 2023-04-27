ba0824143360acdd9091c52099c1d5bc15326c63
Sai Dinesh Kopparthi <S559963@nwmissouri.edu>
2023-04-27 09:40:39 -0500

    Validator for car_cost and buttons for create and update

--- models/car.js
+++ models/car.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const carSchema = mongoose.Schema({
 car_name: String,
 car_make: String,
-car_cost: Number
+car_cost: {
+    type:Number,
+    min: [3000, 'Cost must be at least 3000'],
+    max: [2500000000, 'Cost should not be more than 2500000000']
+}
 })
 module.exports = mongoose.model("Car",
 carSchema)
